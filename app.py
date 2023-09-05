496ff997649420816e2db7cc881fefb397e86c89
moreshk <moreshk@gmail.com>
2023-09-05 14:21:52 +1000

    added langchain

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# Fetch the OPENAI_API_KEY from environment variables
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise ValueError("Please set the OPENAI_API_KEY in the .env file.")
+
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+# from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
